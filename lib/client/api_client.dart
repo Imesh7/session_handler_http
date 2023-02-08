db2f9562a705a77da94caf2c4a1ad351ce2025ce
2019imesh <2019imesh@gmail.com>
2023-02-08 18:36:46 +0530

    update path

--- lib/client/api_client.dart
+++ lib/client/api_client.dart
@@ -1,8 +1,8 @@
 import 'package:http/http.dart' as http;
 
 class ApiClient {
-  static const baseUrl = "http://185.239.208.29:8000/api";
-  static const localUrl = "http://192.168.1.18:3001";
+  static const baseUrl = "http://HOSTNAME/api";
+  static const localUrl = "http://HOSTNAME";
 
   static var client = http.Client();
 }
